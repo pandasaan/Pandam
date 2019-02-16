550d6affa063a9695087d3ffab50e4d6fe3daab3
Genki Sugiyama <sgym0318marcos@gmail.com>
2019-02-16 10:38:12 +0000

    [Modified]cartのamountのデフォルト値を0から1に変更

--- db/migrate/20190216102826_create_carts.rb
+++ db/migrate/20190216102826_create_carts.rb
@@ -3,7 +3,7 @@ class CreateCarts < ActiveRecord::Migration[5.2]
     create_table :carts do |t|
       t.integer :user_id, null: false, default: ""
       t.integer :item_id, null: false, default: ""
-      t.integer :amount, null: false, default: ""
+      t.integer :amount, null: false, default: "1"
       t.timestamps
     end
   end
