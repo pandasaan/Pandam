619b0711846a6e87b7124977abe065ea6ab4ce15
Genki Sugiyama <sgym0318marcos@gmail.com>
2019-02-18 07:54:41 +0000

    cart周りのaction作成（数量調整はまだ）

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,23 @@
 class CartsController < ApplicationController
   def create
+    cart = Cart.new
+    cart.user_id = current_user.id
+    cart.item_id = Item.find(params[:item_id]).id
+    cart.save
+    flash[:notice] = "カートへ保存しました！"
+    redirect_to carts_path
   end
 
   def index
+    @carts = Cart.where(user_id: current_user.id)
   end
 
   def update
   end
 
   def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+    redirect_to carts_path
   end
 end
