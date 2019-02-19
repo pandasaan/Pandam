e3dde27f7995b4a035ab53d66179907426b3f50d
harryshioristyles <imshiori46@gmail.com>
2019-02-19 19:26:27 +0900

    '管理者のヘッダー。購入履歴のorder保存可能。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,50 @@
 class OrdersController < ApplicationController
   def new
+
+    @carts = Cart.where(user_id: current_user.id)
+        total = 0
+        @carts.each do |a|
+          total += a.amount * a.item.price
+        end
+    @total_price = total
+
   end
 
   def create
+
+      carts = Cart.where(user_id: current_user.id)
+        total = 0
+        carts.each do |a|
+          total += a.amount * a.item.price
+        end
+    total_price = total
+
+    name = current_user.name
+    postal_code = "1"
+    address = "1"
+
+    order = Order.new(user_id: current_user.id, order_name: name, order_postal_code: postal_code, order_address: address, total_price: total_price)
+
+    if order.save
+    redirect_to new_order_path
+    flash[:notice] = "保存ok"
+    else
+    redirect_to new_order_path
+    flash[:notice] = "失敗"
+    end
+
   end
 
   def show
+        flash[:notice] = "ご購入ありがとうございます！"
+    redirect_to result_order_path
   end
 
   def index
   end
 
   def result
+    @orders = Order.where(user_id: current_user.id)
   end
 
   def flg_update
@@ -19,4 +52,6 @@ class OrdersController < ApplicationController
 
   def item_flg_update
   end
+
+
 end
