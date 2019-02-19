15886d0b056b6a02627ab9be8899205a79a9e1f6
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-02-19 03:55:26 +0000

    shipmentのindexのviewの編集

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,8 +1,14 @@
 class ShipmentsController < ApplicationController
   def create
+    @shipments = Shipment.new(shipment_params)
+    @shipments.save
+    redirect_to '/shipments'
+
   end
 
   def index
+    @shipments = Shipment.new
+
   end
 
   def edit
@@ -12,5 +18,10 @@ class ShipmentsController < ApplicationController
   end
 
   def destroy
+
+    private
+    def shipment_params
+      params.require(:shipment).permit(:shipment_name, :shipment_name_kana, :shipment_postal_code, :shipment_address)
+    end
   end
 end
