7b15a0060b2bf7c38e4d4b88b804ebea829355cc
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-02-21 10:39:38 +0000

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_paranoid
-  attachment :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
