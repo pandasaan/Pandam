497863b4df7b55814e5b7056216446d8ccdb3f9a
Genki Sugiyama <sgym0318marcos@gmail.com>
2019-02-21 10:17:36 +0000

    [Add]admin用購入履歴一覧、詳細の追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,9 +7,30 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
+  def dlv_flg_update
+    order = Order.find(params[:id])
+    order.update(dlv_status_params)
+    flash[:notice] = "手配状況を更新しました"
+    redirect_to admin_order_path(order.id)
+  end
+
   def flg_update
+    order = Order.find(params[:id])
+    order.update(cancel_params)
+    flash[:notice]  = "キャンセル対応状況を更新しました"
+    redirect_to admin_order_path(order.id)
   end
 
   def item_flg_update
+  end
+
+  private
+
+  def dlv_status_params
+    params.require(:order).permit(:dlv_status)
+  end
+
+  def cancel_params
+    params.require(:order).permit(:cancell_status)
   end
 end
