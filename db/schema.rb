64b01cf0a5805214b4b9767823d6bbff9fd48e78
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-02-26 08:30:58 +0000

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_075518) do
+ActiveRecord::Schema.define(version: 2019_02_26_072055) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_075518) do
     t.integer "cancell_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "orders", force: :cascade do |t|
